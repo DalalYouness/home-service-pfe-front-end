12bcc2ddf13bc003ba745c9d9175491a6dbe9987
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-07-17 16:20:41 +0100

    add axios intercepters

--- src/services/api.client.ts
+++ src/services/api.client.ts
@@ -9,4 +9,52 @@ const apiClient = axios.create({
   },
 });
 
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (!error.response) {
+      console.error("Network Error / Server is Down");
+      return Promise.reject(error);
+    }
+    const { status } = error.response;
+    switch (status) {
+      case 401:
+        console.warn("Session expirée, redirection ou modal...");
+        window.dispatchEvent(new CustomEvent("session-expired"));
+        break;
+
+      case 403:
+        window.location.href = "/403";
+        break;
+
+      case 500:
+        if (error.config.method === "get") {
+          window.location.href = "/500";
+        } else {
+          console.error("Internal Server Error on Action");
+        }
+        break;
+
+      default:
+        break;
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default apiClient;
